d85ed35ce6ef5aa175141e7c7ffd7c2df58e5153
Andrianka <andrianka13@gmail.com>
2013-12-03 17:40:36 +0100

    Added gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def gravatar_url(user)
+		default_url = "#{root_url}images/default.png"
+		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+		"http://gravatar.com/avatar/#{gravatar_id}.png" 
+	end
 end
